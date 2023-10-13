13d5a2ee323ee2597bd0b44fada56061a69f60bc
Steven Vandevelde <steven.vandevelde@hey.com>
2023-10-13 18:49:10 +0200

    WIP

--- site/tailwind.config.js
+++ site/tailwind.config.js
@@ -7,35 +7,41 @@ export default {
     "./views/**/*.gren"
   ],
   theme: {
-    extend: {
-      colors: {
-        "baba-ganoush": "#f2b98c",
-        "bellini": "#f0c8ae",
-        "burrito": "#edd3c3",
-        "chewing-gum": "#ecadaa",
-        "dreamy-candy-forest": "#b28de2",
-        "girl-power": "#d9a3d7",
-        "iridescent": "#3a6255",
-        "larb-gai": "#dcc6a6",
-        "lichen": "#9cc5a9",
-        "medlar": "#d6dac0",
-        "morning-snow": "#f3f4ec",
-        "neon-violet": "#64517d",
-        "paradise-island": "#63b199",
-        "trail-dust": "#cec6aa",
-      },
-      fontFamily: {
-        display: ["Gabarito", "Assistant", ...defaultTheme.fontFamily.sans],
-        sans: ["Assistant", ...defaultTheme.fontFamily.sans],
-        serif: [
-          ["Playfair", ...defaultTheme.fontFamily.serif],
-          {
-            fontVariationSettings: '"opsz" 42, "wdth" 88',
-            letterSpacing: "-0.025em"
-          }
-        ]
-      }
+    colors: {
+      "baba-ganoush": "#f2b98c",
+      "bellini": "#f0c8ae",
+      "bisque": "#fde5c6",
+      "burrito": "#edd3c3",
+      "buttercream": "#f2decd",
+      "chewing-gum": "#ecadaa",
+      "dreamy-candy-forest": "#b28de2",
+      "empress": "#7e7173",
+      "girl-power": "#d9a3d7",
+      "iridescent": "#3a6255",
+      "larb-gai": "#dcc6a6",
+      "lichen": "#9cc5a9",
+      "medlar": "#d6dac0",
+      "morning-snow": "#f3f4ec",
+      "neon-violet": "#64517d",
+      "paradise-island": "#63b199",
+      "perfume-cloud": "#e6cdcf",
+      "pink-glitter": "#fde3d7",
+      "saturn": "#fbe5bc",
+      "silver-bird": "#fbf5f0",
+      "trail-dust": "#cec6aa",
+      "young-apricot": "#fed2b2",
     },
+    fontFamily: {
+      display: ["Gabarito", "Assistant", ...defaultTheme.fontFamily.sans],
+      sans: ["Assistant", ...defaultTheme.fontFamily.sans],
+      serif: [
+        ["Playfair", ...defaultTheme.fontFamily.serif],
+        {
+          fontVariationSettings: '"opsz" 42, "wdth" 88',
+          letterSpacing: "-0.025em"
+        }
+      ]
+    }
   },
   plugins: [],
 }

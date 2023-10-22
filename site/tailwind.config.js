8035e19e0305885b092790bc8f0b2975ec3e5160
Steven Vandevelde <steven.vandevelde@hey.com>
2023-10-22 19:32:52 +0200

    WIP

--- site/tailwind.config.js
+++ site/tailwind.config.js
@@ -1,37 +1,36 @@
-import defaultTheme from "tailwindcss/defaultTheme"
-
+import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./views/**/*.gren"
-  ],
+  content: ["./views/**/*.gren"],
   theme: {
     extend: {
       colors: {
         "baba-ganoush": "#f2b98c",
-        "bellini": "#f0c8ae",
-        "bisque": "#fde5c6",
-        "burrito": "#edd3c3",
-        "buttercream": "#f2decd",
+        bellini: "#f0c8ae",
+        bisque: "#fde5c6",
+        burrito: "#edd3c3",
+        buttercream: "#f2decd",
         "chewing-gum": "#ecadaa",
         "cold-lips": "#3959af4",
         "dreamy-candy-forest": "#b28de2",
-        "empress": "#7e7173",
+        empress: "#7e7173",
+        "french-porcelain": "#f9f6f4",
         "girl-power": "#d9a3d7",
-        "iridescent": "#3a6255",
+        iridescent: "#3a6255",
         "larb-gai": "#dcc6a6",
-        "lichen": "#9cc5a9",
-        "medlar": "#d6dac0",
+        lichen: "#9cc5a9",
+        medlar: "#d6dac0",
         "morning-snow": "#f3f4ec",
         "neon-violet": "#64517d",
         "paradise-island": "#63b199",
         "perfume-cloud": "#e6cdcf",
-        "perrywinkle": "#878ef0",
+        perrywinkle: "#878ef0",
         "pink-glitter": "#fde3d7",
-        "saturn": "#fbe5bc",
+        saturn: "#fbe5bc",
         "silver-bird": "#fbf5f0",
         "trail-dust": "#cec6aa",
+        tuxedo: "#403f42",
         "wondrous-wisteria": "#a3a9f3",
         "young-apricot": "#fed2b2",
       },
@@ -42,11 +41,11 @@ export default {
           ["Playfair", ...defaultTheme.fontFamily.serif],
           {
             fontVariationSettings: '"opsz" 42, "wdth" 88',
-            letterSpacing: "-0.025em"
-          }
-        ]
-      }
-    }
+            letterSpacing: "-0.025em",
+          },
+        ],
+      },
+    },
   },
   plugins: [],
-}
+};
